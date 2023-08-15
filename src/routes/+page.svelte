fb8f46c669cf749473247bf9d44eef3bfec1d61a
sai madhan <saimadhan@outlook.in>
2023-08-15 19:18:40 +0530

    initialized repo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div class=" grid place-items-center container mx-auto min-h-screen w-full">
+	<div>
+		<h1 class="text-5xl tracking-wide font-semibold">Import Your Podcast Library into Spotify</h1>
+		<p class="text-xl my-4 text-center">
+			Simply drop in your <em>podcasts.opml</em> file exported from your podcast provider and let us
+			take care of the rest
+		</p>
+	</div>
+	<button type="button" class="btn btn-wide btn-circle btn-primary">Lets Go!1</button>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+const daisyui = require('daisyui');
+const typography = require('@tailwindcss/typography');
+
+/** @type {import('tailwindcss').Config}*/
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [typography, daisyui],
+
+	daisyui: {
+		themes: ['black']
+	}
+};
+
+module.exports = config;
