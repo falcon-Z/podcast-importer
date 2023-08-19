55652f5f1af90a6d7557512cc414058469dbfc30
sai madhan <saimadhan@outlook.in>
2023-08-19 11:52:15 +0530

    added error handling

--- src/routes/(import)/[stage]/+layout.ts
+++ src/routes/(import)/[stage]/+layout.ts
@@ -1,11 +1,18 @@
+import { error } from '@sveltejs/kit';
 import type { LayoutLoad } from './$types';
 
-export const load = (async () => {
+export const load = (async ({ params }) => {
 	let steps = [
 		{ step: 1, name: 'Upload Library' },
 		{ step: 2, name: 'Searching for Podcasts' },
 		{ step: 3, name: 'Subscribing' }
 	];
+
+	const step = steps.find((s) => s.step === parseInt(params.stage));
+
+	if (!step) {
+		throw error(404, 'Not found');
+	}
 
 	return { steps };
 }) satisfies LayoutLoad;
