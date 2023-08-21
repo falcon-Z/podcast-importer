a2f1ff44144045d922d526475fb4403d39d51228
sai madhan <saimadhan@outlook.in>
2023-08-21 15:13:20 +0530

    transitions for errors

--- src/routes/(import)/[stage]/components/filePicker.svelte
+++ src/routes/(import)/[stage]/components/filePicker.svelte
@@ -2,16 +2,19 @@
 	// @ts-nocheck
 
 	import Dropzone from 'svelte-file-dropzone/Dropzone.svelte';
+	import { slide } from 'svelte/transition';
 
 	$: fileErrors = null;
 	$: acceptedFile = null;
 
-	const handleFile = (e) => {
+	const handleFile = async (e) => {
 		const { acceptedFiles } = e.detail;
 
 		if (acceptedFiles.length > 0) {
 			fileErrors = null;
 			acceptedFile = acceptedFiles[0].name;
+
+			const fileText = await acceptedFiles[0].text();
 		}
 	};
 	const handleRejetion = (e) => {
@@ -32,13 +35,16 @@
 />
 
 {#if acceptedFile}
-	<dov class="alert rounded-xl my-4">
+	<dov class="alert rounded-xl my-4" transition:slide={{ delay: 200 }}>
 		{acceptedFile}
 	</dov>
 {/if}
 
 {#if fileErrors}
-	<ul class="flex flex-col rounded-xl gap-4 justify-start my-4 alert alert-error">
+	<ul
+		class="flex flex-col rounded-xl gap-4 justify-start my-4 alert alert-error"
+		transition:slide={{ delay: 200 }}
+	>
 		{#each fileErrors as error}
 			<li>{error.message}</li>
 		{/each}
